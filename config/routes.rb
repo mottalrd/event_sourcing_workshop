ead608b74b517af2aa7e5b58bbd63997fcd6409a
Alfredo Motta <alfredo@creditspring.co>
2018-10-06 08:11:55 +0100

    add rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  resources :articles
+
+  root 'welcome#index'
 end
